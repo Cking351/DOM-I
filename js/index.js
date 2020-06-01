ffed797711b888220abdecfc8d4867ec260e090d
Chris King <cking5836@gmail.com>
2020-06-01 16:11:05 -0400

    Starting on contact section of JS

--- js/index.js
+++ js/index.js
@@ -82,5 +82,42 @@ const ctaH1 = document.getElementsByTagName('h1')[0];
 ctaH1.textContent = siteContent['cta']['h1'];
 
 
-// Main Content
-const featuresHeading = document
+// Main Content Headings
+const featuresHeading = document.getElementsByTagName('h4')[0];
+featuresHeading.textContent = siteContent['main-content']['features-h4'];
+
+const aboutHeading = document.getElementsByTagName('h4')[1];
+aboutHeading.textContent = siteContent['main-content']['about-h4'];
+
+const servicesHeading = document.getElementsByTagName('h4')[2];
+servicesHeading.textContent = siteContent['main-content']['services-h4'];
+
+const productHeading = document.getElementsByTagName('h4')[3];
+productHeading.textContent = siteContent['main-content']['product-h4'];
+
+const visionHeading = document.getElementsByTagName('h4')[4];
+visionHeading.textContent = siteContent['main-content']['vision-h4'];
+
+// Main Content Picture
+const mainImage = document.getElementById('middle-img');
+mainImage.setAttribute('src', siteContent["main-content"]["middle-img-src"])
+
+// Main Content Paragraphs
+
+const featuresParagraph = document.getElementsByTagName('p')[0];
+featuresParagraph.textContent = siteContent['main-content']["features-content"]
+
+const aboutParagraph = document.getElementsByTagName('p')[1];
+aboutParagraph.textContent = siteContent['main-content']["about-content"]
+
+const servicesParagraph = document.getElementsByTagName('p')[2];
+servicesParagraph.textContent = siteContent['main-content']["services-content"]
+
+const productParagraph = document.getElementsByTagName('p')[3];
+productParagraph.textContent = siteContent['main-content']["product-content"]
+
+const visionParagraph = document.getElementsByTagName('p')[4];
+visionParagraph.textContent = siteContent['main-content']["vision-content"]
+
+// Contact
+
